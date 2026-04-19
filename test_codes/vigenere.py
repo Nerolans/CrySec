8f4756cb436a69bd64fa4318b455cda8cdc73753
Guillaume Hyvert <guillaume.hyvert@ik.me>
2026-04-19 12:29:04 +0200

    vigenere decode (to be tested)

--- test_codes/vigenere.py
+++ test_codes/vigenere.py
@@ -1,11 +1,22 @@
 def vigenere_encode(msg, key):
     result = ''
     for i in range(len(msg)):
+        #each chr of the message
         char = msg[i]
+        #extends the key if it's shorter than the message (repeats itself)
         key_char = key[i % len(key)]
-
+        #get the character according to the vigenere table (basically adds both the key anf the chr number and laps around the alphabet if it's greater then 26)
         new_code = (ord(char) + ord(key_char)) % 256
         result += chr(new_code)
 
     return result
 
+def vigenere_decode(msg, key):
+    result = ''
+    for i in range(len(msg)):
+        char = msg[i]
+        key_char = key[i % len(key)]
+        #same as above but subtract the key value instead of adding it
+        new_code = (ord(char) - ord(key_char)) % 256
+        result += chr(new_code)
+    return result
