5b9e9bfe24e2680975fc9108e6c94489efc28619
Guillaume Hyvert <guillaume.hyvert@ik.me>
2026-04-24 21:57:07 +0200

    finished diffie-hellman (to check when server is up

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -9,6 +9,7 @@ from test_codes.RSA import generate_keys, rsa_encode, transform_encoded_byte, rs
 from test_codes.cypher import shift_encode, shift_findKey, shift_decode
 from test_codes.vigenere import vigenere_encode
 from test_codes.hash import *
+from test_codes.Diffie import *
 from clientConnect import *
 from Payload import PayloadHandler, PayloadType
 
@@ -53,6 +54,19 @@ class ProjetCrypto(QtWidgets.QMainWindow):
         self.btn_rsa_decode.clicked.connect(self.btn_decode_rsa)
         self.btn_rsa_generate.clicked.connect(self.btn_generate_rsa)
         #-------------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------------
+        # Bouton dans la frame Diffie
+        self.btn_mode_diffie.clicked.connect(self.activer_diffHel)
+        self.btn_DiffHel_connect.clicked.connect(self.btn_connect_diffHel)
+
+        self.btn_DiffHel_part1_Send.clicked.connect(self.btn_part1_diffHel)
+        self.btn_DiffHel_part2_Send.clicked.connect(self.btn_part2_diffHel)
+        self.btn_DiffHel_part3_Send.clicked.connect(self.btn_part3_diffHel)
+
+        self.btn_DiffHel_Generate1.clicked.connect(self.btn_first_diffHel)
+        self.btn_DiffHel_Generate2.clicked.connect(self.btn_second_diffHel)
+        self.btn_DiffHel_Generate3.clicked.connect(self.btn_final_diffHel)
+        # -------------------------------------------------------------------------------------------------------------------
     def activer_shift(self):
         self.page_crypto.setCurrentIndex(0)
         self.print_server.clear()
@@ -76,6 +90,11 @@ class ProjetCrypto(QtWidgets.QMainWindow):
         self.print_server.clear()
         self.rsa_text.clear()
         self.print_server.append("Mode RSA sélectionné")
+
+    def activer_diffHel(self):
+        self.page_crypto.setCurrentIndex(4)
+        self.print_server.clear()
+        self.print_server.append("Mode Diffie-hellman sélectionné")
 
     def activer_chat(self):
         self.page_crypto.setCurrentIndex(5)
@@ -209,6 +228,39 @@ class ProjetCrypto(QtWidgets.QMainWindow):
         self.rsa_publickey.setText(str(publickey))
         self.rsa_privatekey.setText(str(privatekey))
 
+
+#-------------------------------------------------------------------------------------------------------------------
+# Fonction dans la frame Diffie-Hellman
+#-------------------------------------------------------------------------------------------------------------------
+    def btn_first_diffHel(self):
+        prime = generate_prime(1000, int(self.DiffHel_max.text()))
+        generator = find_p_root(prime)
+        self.DiffHel_prime.setText(str(prime))
+        self.DiffHel_generator.setText(str(generator))
+
+    def btn_second_diffHel(self):
+        private = generate_private()
+        self.DiffHel_key_private.setText(str(private))
+        a_public = generate_publicKey(self.DiffHel_prime.text(), self.DiffHel_generator.text(), private)
+        self.DiffHel_key_public_a.setText(str(a_public))
+
+    def btn_final_diffHel(self):
+        secret = calculate_secret(int(self.DiffHel_key_public_b.text()), int(self.DiffHel_key_private.text()), int(self.DiffHel_prime.text()))
+        self.DiffHel_secret.setText(str(secret))
+
+
+    #Server
+    def btn_connect_diffHel(self):
+        run_server_diffie("init", self.print_server, self)
+
+    def btn_part1_diffHel(self):
+        run_server_diffie("first", self.print_server, self)
+
+    def btn_part2_diffHel(self):
+        run_server_diffie("second", self.print_server, self)
+
+    def btn_part3_diffHel(self):
+        run_server_diffie("third", self.print_server, self)
 
 # --- Lancement ---
 if __name__ == '__main__':
